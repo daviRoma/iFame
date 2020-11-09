70c855dc9eebd10bd97c5fb52229a1e3d7d503dd
GIordano <giordano.daloisio@gmail.com>
2020-11-09 11:02:29 +0100

    add pages

---
+++ src/pages/EventListPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function EventListPage() {
+  return (
+    <View>
+      <Text>EventListPage</Text>
+    </View>
+  );
+}
---
+++ src/pages/SignInPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SignInPage() {
+  return (
+    <View>
+      <Text>SignInPage</Text>
+    </View>
+  );
+}
