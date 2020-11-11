9043ec795dab68521d93c484aacf121f3c41e55b
GIordano <giordano.daloisio@gmail.com>
2020-11-11 12:03:36 +0100

    fix

--- App.js
+++ App.js
@@ -8,9 +8,9 @@
 import 'react-native-gesture-handler';
 import 'react-native-paper';
 import React from 'react';
-import RootNavigator from '../navigators/RootNavigator';
+import RootNavigator from './src/navigators/RootNavigator';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './src/app/store';
 
 const App = () => {
   return (
